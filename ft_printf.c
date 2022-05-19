0a0ed8c31e786d745a5eb9fd458c82970e211145
pbouillo <pbouillo@student.42.fr>
2022-05-19 18:00:57 +0200

    nc

--- ft_printf.c
+++ ft_printf.c
@@ -6,10 +6,20 @@
 /*   By: pbouillo <pbouillo@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 13:31:06 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/05/11 14:23:41 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/05/19 17:35:24 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	va_start(args, format);
+	t_print	print;
+	print.len = 0;
+	print.width = 0;
+	
+	
+	return (print.len);
+}
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: pbouillo <pbouillo@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 13:32:22 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/05/19 16:54:54 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/05/19 17:24:36 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@
 # include <stdarg.h>
 # include "libft/libft.h"
 
-int		ft_printf(const char *, ...);
+typedef struct s_print
+{
+	int	len;
+	int	width;
+}				t_print;
+
+int		ft_printf(const char *format, ...);
 char	*ft_strchr_perc(const char *s);
-
-
 
 #endif
