257c18c8ba601c1de5297efe47460ed59f5ebe6d
pbouillo <pbouillo@student.42.fr>
2022-05-24 11:41:17 +0200

    nc

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pbouillo <pbouillo@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 13:31:06 by pbouillo          #+#    #+#             */
-/*   Updated: 2022/05/19 17:35:24 by pbouillo         ###   ########.fr       */
+/*   Updated: 2022/05/24 11:04:55 by pbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_printf(const char *format, ...)
 {
-	va_list	args;
-	va_start(args, format);
+	va_list	ap;
+	va_start(ap, format);
 	t_print	print;
 	print.len = 0;
 	print.width = 0;
